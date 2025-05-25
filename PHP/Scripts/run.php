240fe0084d2734a3804ac49aa4e7a31d6d6a8ca0
jeanmfdias <jeanmfdias@gmail.com>
2025-05-25 19:14:51 -0300

    feat: refactory script

--- PHP/Scripts/run.php
+++ PHP/Scripts/run.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Jean\Scripts\Download;
 
 $download = new Download();
-$download->downloadImagesFromUrl('http://www.url.com', __DIR__ . '/tmp/');
+$download->downloadImagesFromUrl('https://www.pornpics.com/galleries/atk-girlfriends-performed-by-serena-hill-hot-pics-92797090/', __DIR__ . '/tmp/');
--- PHP/Scripts/src/Download.php
+++ PHP/Scripts/src/Download.php
@@ -11,6 +11,79 @@ class Download
 {
     public function downloadImagesFromUrl(string $url, string $basePath): void
     {
+        $this->createDirectory($basePath);
+        $this->createDirectory($basePath . "images/");
+
+        $path = $this->saveHtmlFile($basePath, $url);
+
+        $html = file_get_contents($path);
+        if ($html === false) {
+            throw new RuntimeException("Failed to read the file at $path");
+        }
+        $dom = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($html);
+        libxml_clear_errors();
+        
+        $total = $this->getTotalImages($dom);
+        $i = 1;
+        foreach ($dom->getElementsByTagName('a') as $a) {
+            if ($a->hasAttribute('class') && $a->getAttribute('class') === 'rel-link') {
+                $href = $a->getAttribute('href');
+                $this->downloadImage($basePath, $href, $total, $i);
+            }
+        }
+    }
+
+    private function createDirectory(string $path): void
+    {
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+
+    private function downloadImage($basePath, $href, $total, &$i): void
+    {
+        if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $href)) {
+            $imagePath = $basePath . "images/" . basename($href);
+            if (!file_exists($imagePath)) {
+                $imageCh = curl_init($href);
+                $imageFp = fopen($imagePath, 'wb');
+
+                curl_setopt($imageCh, CURLOPT_FILE, $imageFp);
+                curl_setopt($imageCh, CURLOPT_HEADER, 0);
+
+                if (curl_exec($imageCh) === false) {
+                    throw new RuntimeException("Failed to download image from $href: " . curl_error($imageCh));
+                }
+
+                curl_close($imageCh);
+                fclose($imageFp);
+                echo "$i/$total...\n";
+                $i++;
+            } else {
+                echo "Image already exists: $imagePath\n";
+            }
+        }
+    }
+
+    private function getTotalImages($dom): int
+    {
+        $total = 0;
+        foreach ($dom->getElementsByTagName('a') as $a) {
+            if ($a->hasAttribute('class') && $a->getAttribute('class') === 'rel-link') {
+                $total++;
+            }
+        }
+        if ($total === 0) {
+            throw new RuntimeException("No links found with class 'rel-link' in the HTML content.");
+        }
+        echo "Total links found: $total\n";
+        return $total;
+    }
+
+    private function saveHtmlFile($basePath, $url): string
+    {
         $path = $basePath . "file.html";
         $ch = curl_init($url);
         $fp = fopen($path, 'wb');
@@ -22,52 +95,6 @@ class Download
         curl_close($ch);
         fclose($fp);
 
-        $html = file_get_contents($path);
-        if ($html === false) {
-            throw new RuntimeException("Failed to read the file at $path");
-        }
-        $dom = new DOMDocument();
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($html);
-        libxml_clear_errors();
-        
-
-        $total = 0; //$dom->getElementsByTagName('a')->length;
-        foreach ($dom->getElementsByTagName('a') as $a) {
-            if ($a->hasAttribute('class') && $a->getAttribute('class') === 'rel-link') {
-                $total++;
-            }
-        }
-        if ($total === 0) {
-            throw new RuntimeException("No links found with class 'rel-link' in the HTML content.");
-        }
-        echo "Total links found: $total\n";
-        $i = 1;
-        foreach ($dom->getElementsByTagName('a') as $a) {
-            if ($a->hasAttribute('class') && $a->getAttribute('class') === 'rel-link') {
-                $href = $a->getAttribute('href');
-                if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $href)) {
-                    $imagePath = $basePath . "images/" . basename($href);
-                    if (!file_exists($imagePath)) {
-                        $imageCh = curl_init($href);
-                        $imageFp = fopen($imagePath, 'wb');
-
-                        curl_setopt($imageCh, CURLOPT_FILE, $imageFp);
-                        curl_setopt($imageCh, CURLOPT_HEADER, 0);
-
-                        if (curl_exec($imageCh) === false) {
-                            throw new RuntimeException("Failed to download image from $href: " . curl_error($imageCh));
-                        }
-
-                        curl_close($imageCh);
-                        fclose($imageFp);
-                        echo "$i/$total...\n";
-                        $i++;
-                    } else {
-                        echo "Image already exists: $imagePath\n";
-                    }
-                }
-            }
-        }
+        return $path;
     }
 }
