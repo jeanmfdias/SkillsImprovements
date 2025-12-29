418bd2aadbc83168a5f283175a8a4a2d67cac197
jeanmfdias <jeanmfdias@gmail.com>
2025-12-29 14:08:35 -0300

    feat: add function to check the word

--- CPP/hangmanGame.cpp
+++ CPP/hangmanGame.cpp
@@ -3,8 +3,31 @@
 
 using namespace std;
 
-int main() {
-    string secretWord = "WATERMELON";
+string secretWord = "WATERMELON";
 
+bool wordIsPresent(char word) {
+    for (int i = 0; i < secretWord.size(); i++) {
+        if (secretWord[i] == word) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
     cout << secretWord << endl;
+
+    bool noHit = true;
+    bool noLose = true;
+
+    while (noHit && noLose) {
+        char shot;
+        cin >> shot;
+
+        if (wordIsPresent(shot)) {
+            cout << "You were it right!" << endl;
+        } else {
+            cout << "You were wrong" << endl;
+        }
+    }
 }
