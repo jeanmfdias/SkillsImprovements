b3394cd53f1ec0925692c106ef73b8b3517c9bd3
jeanmfdias <jeanmfdias@gmail.com>
2026-01-01 16:12:23 -0300

    feat: add shots preview

--- CPP/hangmanGame.cpp
+++ CPP/hangmanGame.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 string secretWord = "WATERMELON";
+
+map<char, bool> shots;
 
 bool wordIsPresent(char shot) {
     for (char letter : secretWord) {
@@ -15,17 +18,25 @@ bool wordIsPresent(char shot) {
 }
 
 int main() {
-    cout << secretWord << endl;
-
     bool noHit = true;
     bool noLose = true;
 
     while (noHit && noLose) {
+        for (char letter : secretWord) {
+            if (shots[letter]) {
+                cout << letter << " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char shot;
         cin >> shot;
 
         if (wordIsPresent(shot)) {
             cout << "You were it right!" << endl;
+            shots[shot] = true;
         } else {
             cout << "You were wrong" << endl;
         }
