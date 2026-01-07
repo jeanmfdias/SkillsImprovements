a2ba42c2eb7518363b7c25ae5a30a1e766761258
jeanmfdias <jeanmfdias@gmail.com>
2026-01-07 00:03:16 -0300

    feat: create and list accounts

--- CPP/bankingAccount.cpp
+++ CPP/bankingAccount.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <list>
+#include <map>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 list<string> names;
-list<double> balances;
+map<string, double> balances;
 
 void separator(string symbol, int quantity) {
     string print;
@@ -40,7 +43,41 @@ void showMenu() {
 
     formatMessage("Menu", "-", 48);
     cout << "+ 0 - Exit" << endl;
+    cout << "+ 1 - Create a account" << endl;
+    cout << "+ 2 - List all accounts" << endl;
     cout << "+ Choice a option: ";
+}
+
+void sleep(int seconds) {
+    this_thread::sleep_for(chrono::seconds(seconds));
+}
+
+void createAccount() {
+    system("clear");
+    cout << "Name: ";
+    string name;
+    cin >> name;
+    names.push_back(name);
+    cout << "Initial balance: ";
+    float amount;
+    cin >> amount;
+    balances.insert({name, amount});
+    system("clear");
+    formatMessage("New client created!", "+", 30);
+    sleep(2);
+}
+
+void listAllAccounts() {
+    system("clear");
+    int id = 1;
+    for (string name : names) {
+        cout << "--- Account " << id++ << endl;
+        cout << "Name: " << name << endl;
+        cout << "Balance: " << balances[name] << endl;
+    }
+    cout << "Type 0 to continue..." << endl;
+    string enter;
+    cin >> enter;
 }
 
 int main() {
@@ -49,6 +86,17 @@ int main() {
         system("clear");
         showMenu();
         cin >> choice;
+
+        switch (choice) {
+            case 1:
+                createAccount();
+                break;
+            case 2:
+                listAllAccounts();
+                break;
+            default:
+                cout << "Invalid option!" << endl;
+        }
     }
     system("clear");
     cout << "Bye!" << endl;
